abad4e14df1b09e6dbe6ce84197214010c1c8c17
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-08 08:34:13 -0700

    Renaming project to generic relay.

---
+++ BigMission.Shared/Auth/EncryptionHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BigMission.Shared.Auth;
+
+public static class EncryptionHelper
+{
+    public static string EncryptString(string plainText, string key)
+    {
+        using Aes aesAlg = Aes.Create();
+        aesAlg.Key = Encoding.UTF8.GetBytes(key);
+        aesAlg.IV = new byte[16]; // Initialization vector with zeros
+
+        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+        using MemoryStream msEncrypt = new();
+        using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
+        using StreamWriter swEncrypt = new(csEncrypt);
+        swEncrypt.Write(plainText);
+        return Convert.ToBase64String(msEncrypt.ToArray());
+    }
+
+    public static string DecryptString(string cipherText, string key)
+    {
+        using Aes aesAlg = Aes.Create();
+        aesAlg.Key = Encoding.UTF8.GetBytes(key);
+        aesAlg.IV = new byte[16]; // Initialization vector with zeros
+
+        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+        using MemoryStream msDecrypt = new(Convert.FromBase64String(cipherText));
+        using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using StreamReader srDecrypt = new(csDecrypt);
+        return srDecrypt.ReadToEnd();
+    }
+}
