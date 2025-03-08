63f6986c78e5125c269db9e7374880eb53805992
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-08 09:08:19 -0700

    Moving KeycloakServiceAuthenticator to shared.

---
+++ BigMission.Shared/Auth/KeycloakServiceAuthenticator.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace BigMission.Shared.Auth;
+
+/// <summary>
+/// Authenticator for Keycloak based rest sharp services.
+/// </summary>
+/// <param name="token"></param>
+/// <param name="authUrl"></param>
+/// <param name="realm"></param>
+/// <param name="clientId"></param>
+/// <param name="clientSecret"></param>
+public class KeycloakServiceAuthenticator(string token, string authUrl, string realm, string clientId, string clientSecret) : AuthenticatorBase(token)
+{
+    protected override async ValueTask<Parameter> GetAuthenticationParameter(string accessToken)
+    {
+        if (string.IsNullOrEmpty(Token) || !IsTokenValid())
+        {
+            Token = await KeycloakServiceToken.RequestClientToken(authUrl, realm, clientId, clientSecret) ?? string.Empty;
+        }
+        return new HeaderParameter(KnownHeaders.Authorization, $"Bearer {Token}");
+    }
+
+    private bool IsTokenValid()
+    {
+        try
+        {
+            return KeycloakServiceToken.CheckTokenIsValid(Token);
+        }
+        catch { }
+        return false;
+    }
+}
