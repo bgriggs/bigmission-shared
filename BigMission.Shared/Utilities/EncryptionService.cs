89b3dc6f45851183aeea0f6d8b805227c4990003
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-09 13:18:16 -0600

    Adding shared configuration service. Adding X2 config.

---
+++ BigMission.Shared/Utilities/EncryptionService.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BigMission.Shared.Utilities;
+
+public class EncryptionService
+{
+    private readonly byte[] key;
+    private readonly byte[] iv;
+
+    public EncryptionService(string key, string iv)
+    {
+        if (key.Length != 32 || iv.Length != 16)
+            throw new ArgumentException("Key must be 32 bytes and IV must be 16 bytes long.");
+
+        this.key = Encoding.UTF8.GetBytes(key);
+        this.iv = Encoding.UTF8.GetBytes(iv);
+    }
+
+
+    public string Encrypt(string plainText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                {
+                    swEncrypt.Write(plainText);
+                }
+
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    public string Decrypt(string cipherText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+            {
+                return srDecrypt.ReadToEnd();
+            }
+        }
+    }
+}
